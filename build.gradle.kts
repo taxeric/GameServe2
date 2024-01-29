a07aaa7dd0fe23162822b529b7995f7cc6d58503
幻弦让叶 <1248389474@qq.com>
2024-01-29 15:26:44 +0800

    引入开发库

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,21 @@ dependencies {
 
     // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-websocket
     implementation("org.springframework.boot:spring-boot-starter-websocket:3.2.0")
+
+    // https://mvnrepository.com/artifact/org.mybatis.spring.boot/mybatis-spring-boot-starter
+    implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:3.0.3")
+
+    // https://mvnrepository.com/artifact/org.mybatis/mybatis
+    implementation("org.mybatis:mybatis:3.5.12")
+
+    // https://mvnrepository.com/artifact/org.mybatis/mybatis-spring
+    implementation("org.mybatis:mybatis-spring:3.0.3")
+
+    // https://mvnrepository.com/artifact/com.mysql/mysql-connector-j
+    implementation("com.mysql:mysql-connector-j:8.3.0")
+
+    // https://mvnrepository.com/artifact/org.springframework/spring-jdbc
+    implementation("org.springframework:spring-jdbc:6.1.3")
 }
 
 tasks.withType<KotlinCompile> {
