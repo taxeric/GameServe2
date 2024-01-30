8f6f6bdce35a2d94117a5ac28ccf791f7a4492c4
幻弦让叶 <1248389474@qq.com>
2024-01-30 23:48:38 +0800

    配置

--- src/main/kotlin/org/lanier/gameserve2/GameServe2Application.kt
+++ src/main/kotlin/org/lanier/gameserve2/GameServe2Application.kt
@@ -1,9 +1,13 @@
 package org.lanier.gameserve2
 
+import org.mybatis.spring.annotation.MapperScan
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.transaction.annotation.EnableTransactionManagement
 
 @SpringBootApplication
+@MapperScan(basePackages = ["org.lanier.gameserve2.mapper"])
+@EnableTransactionManagement
 class GameServe2Application
 
 fun main(args: Array<String>) {
