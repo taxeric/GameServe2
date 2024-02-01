44c89902a4bc9c52cb8e9ad5f31302955689a189
幻弦让叶 <1248389474@qq.com>
2024-02-02 01:02:09 +0800

    新增搜索背包service

---
+++ src/main/kotlin/org/lanier/gameserve2/mapper/BackpackMapper.kt
@@ -0,0 +1,19 @@
+package org.lanier.gameserve2.mapper
+
+import org.apache.ibatis.annotations.Mapper
+import org.lanier.gameserve2.entity.Backpack
+
+/**
+ * Created by 幻弦让叶
+ * Date 2024/2/2 0:48
+ */
+@Mapper
+interface BackpackMapper {
+
+    fun findPropById(
+        userId: Int,
+        petId: Int,
+        propId: Int,
+        propType: Int
+    ) : List<Backpack>
+}
